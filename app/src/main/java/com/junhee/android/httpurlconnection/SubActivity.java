b8ae4107f1c22735f110b8fa98182ffcffcafce5
JunHee Lee <jhlee910609@gmail.com>
2017-06-13 01:09:31 +0900

    Dividing class and making interface.

---
+++ app/src/main/java/com/junhee/android/httpurlconnection/SubActivity.java
@@ -0,0 +1,26 @@
+package com.junhee.android.httpurlconnection;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SubActivity extends AppCompatActivity implements TaskInterface{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub);
+
+        Task.newTask(this);
+    }
+
+    @Override
+    public String getUrl() {
+        return null;
+    }
+
+    @Override
+    public void postExecute(String result) {
+        // button.. progress...
+
+    }
+}
